9c8de3c39e704943f124b9fca2c84ce76a1d23ac
gavronski <p.gawronski@ideo.pl>
2022-10-10 01:12:47 +0200

    add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,12 @@ package config
 import (
 	"html/template"
 	"log"
+	"myapp/internal/models"
 
 	"github.com/alexedwards/scs/v2"
 )
 
-//App config holds the application config
+// App config holds the application config
 type AppConfig struct {
 	UseCache      bool
 	TemplateCache map[string]*template.Template
@@ -15,4 +16,5 @@ type AppConfig struct {
 	InfoLog       *log.Logger
 	ErrorLog      *log.Logger
 	Session       *scs.SessionManager
+	MailChan      chan models.MailData
 }
